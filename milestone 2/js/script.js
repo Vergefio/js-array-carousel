e58344491be20b47b2f016aef44439031361665e
Vergefio <riccardods@live.it>
2022-10-23 14:34:15 +0200

    correzione milestone2

--- milestone 2/js/script.js
+++ milestone 2/js/script.js
@@ -1,27 +1,62 @@
+console.log(`ciaooooo`);
+
 const images = [
-  '01.jpg',
-  '02.jpg',
-  '03.jpg',
-  '04.jpg',
-  '05.jpg',
+  `01.jpg`, `02.jpg`, `03.jpg`, `04.jpg`, `05.jpg`
 ];
 
-let imagesTags = '';
+let counterImages = 0;
+let imageTags = ``;
 
-const slider = document.querySelector('.slider');
+//contenitore
+const objectWrapper = document.querySelector(`.objects-wrapper`);
 
-for (let i = 0; i < imagesArray.lenght; i++) {
-  imagesTags += `
-  <img class="item" src="img/${imagesArray[i]}" alt="${imagesArray[i]}">
+//ciclare array
+
+for (let i = 0; i < images.length; i++) {
+  imageTags += `
+  <img class="object" src="img/${images[i]}" alt="${images[i]}">
   `;
 }
 
-let counterImages = 0;
+console.log(objectWrapper);
 
-const next = document.querySelector('.top');
-const prev = document.querySelector('.bottom');
-console.log(next);
 
-slider.innerHTML += imagesTags;
 
-const items = document.getElementsByClassName('item');
+// bottoni
+const next = document.querySelector(`.bottom`);
+const previous = document.querySelector(`.top`);
+previous.classList.add("hide")
+
+//image tags
+
+objectWrapper.innerHTML += imageTags;
+
+const objects = document.getElementsByClassName(`object`)
+
+objects[counterImages].classList.add(`active`);
+
+//click cambiamento img
+
+next.addEventListener("click", function () {
+  objects[counterImages].classList.remove(`active`);
+  objects[++counterImages].classList.add(`active`);
+
+  previous.classList.remove(`hide`);
+  if (counterImages === images.lenght - 1) {
+    next.classList.add(`hide`)
+  }
+
+});
+
+previous.addEventListener("click", function () {
+  objects[counterImages].classList.remove(`active`);
+  objects[--counterImages].classList.add(`active`);
+
+  next.classList.remove(`hide`);
+  if (counterImages === images.lenght - 1) {
+    next.classList.add(`hide`)
+  }
+
+});
+
+console.log(objects);
